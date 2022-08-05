78d5949280d43e960a87680d20a99b85bbc1f979
EmilienDevopsGre <102517933+EmilienDevopsGre@users.noreply.github.com>
2022-08-05 16:56:47 +0200

    encours

---
+++ src/Characters.java
@@ -0,0 +1,11 @@
+public class Characters {
+    public static void main(String[] args) {
+        int life;
+        int force;
+        String weapon;
+        String shield;
+        String spell;
+        String potion;
+
+    }
+}
